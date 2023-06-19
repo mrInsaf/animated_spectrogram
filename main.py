62728e7ba3dbcf338adecc2a99c5b2557ec98990
mrInsaf <mr.insaf@list.ru>
2023-06-20 01:51:18 +0300

    added analysis

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import tkinter as tk
 from spectrogram_animation import *
+from tkinter import *
 
+root = Tk()
+root.mainloop()
 
 
 if __name__ == "__main__":
--- spectrogram_animation.py
+++ spectrogram_animation.py
@@ -1,22 +1,33 @@
+import time
+
 import pyaudio
 import struct
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import signal
 from matplotlib.animation import FuncAnimation
+import tkinter as tk
+from matplotlib.widgets import Slider
+import matplotlib.colors as colors
+import tkinter as tk
+from time import sleep
+
+
 
 CHUNK = 1024 * 3
 FORMAT = pyaudio.paInt16
 CHANNELS = 1
 RATE = 44100
-NUM_COLUMNS = 80
-COLUMN_WIDTH = 1
+NUM_COLUMNS = 60
 MAX_FREQUENCY = 22000
-VMIN = -50
-VMAX = 3
+VMIN = -100
+VMAX = 30
+NOISE_THRESHOLD = -40  # Пороговое значение для шума
+
 
 target_frequency = 50
 tolerance = 20
+times = (0.04643991, 0.02321995)
 
 
 p = pyaudio.PyAudio()
@@ -31,109 +42,161 @@ stream = p.open(
 )
 
 fig, ax = plt.subplots()
+ax_slider = plt.axes([0.2, 0.03, 0.6, 0.03])  # Позиция ползунка на окне
+brightness_slider = Slider(ax_slider, 'Нижний порог', -100, -30, valinit=VMIN)  # Создание ползунка
 
-frequencies, times, spectrogram_data = signal.spectrogram(np.zeros(CHUNK), fs=RATE, window='hann', nperseg=2048, noverlap=1024)
+
+
+
+frequencies, _, spectrogram_data = signal.spectrogram(np.zeros(CHUNK), fs=RATE, window='hann', nperseg=2048, noverlap=1024)
 spectrogram = ax.imshow(spectrogram_data[:int(MAX_FREQUENCY)], aspect='auto', origin='lower',
                         extent=[times[0], times[-1], frequencies[0], frequencies[-1]], vmax=VMAX, vmin=VMIN)
 
+
 plt.xlabel("Время (секунды)")
 plt.ylabel("Частота (Гц)")
-plt.title("Спектрограмма")
 plt.colorbar(spectrogram, label="Амплитуда")
 
 columns = np.zeros((spectrogram_data.shape[0], NUM_COLUMNS))
 
+rec_dur = 0
+j = 0
+hist_db = []
 
-# class HarmRect:
-#     def __init__(self, init_rect_x=0.04, init_rect_y=0, rect_width = 0.001, rect_height = 500):
-#         self.rect_x = init_rect_x  # Координата x (время) квадрата
-#         self.rect_y = init_rect_y  # Начальная координата y (частота) квадрата
-#         self.width = rect_width  # Ширина квадрата
-#         self.height = rect_height  # Высота квадрата (диапазон частоты)
-#
-#     def show_info(self):
-#         print('rect_x=', self.rect_x, ' rect_y=', self.rect_y, ' width=', self.width, ' height=', self.height)
+def nearest(lst, target):
+  nearest_value = min(lst, key=lambda x: abs(x-target))
+  return lst.index(nearest_value)
 
+# Создание окна
+window = tk.Tk()
+pause = False
+# Функция, вызываемая при нажатии кнопки "Анализировать"
+def analyze_button_click():
+    global pause
+    h = 20
+    pause ^= True
+    rect_width = (times[0] - times[1]) / NUM_COLUMNS # Ширина квадрата (длительность временного сегмента)
+    rect_x = times[0]
+    rect_y = 0
+    print(rect_width)
+    # rect_height = hist_db[0][0][1] - hist_db[0][0][0] # Высота квадрата (диапазон частоты)
+    rect_height = 1000
+    if pause:
+        rects = []
+        for i, col in enumerate(hist_db[-NUM_COLUMNS:]):
+            rect_x -= rect_width
+            rect_y = 0
+            # rect_height = 0
+            amps = []
+            for lst in col:
+                amps.append(np.average(lst))
+            np_amps = np.array(amps)
+            noise_freqs = np.where(np_amps < -180)
+            for noise_freq in noise_freqs:
+                if np.array_equal(noise_freq, np.arange(0, 1025)):
+                    rect_xy = (rect_x, rect_y)
+                    rect_height = 20000
+                    rect = plt.Rectangle(rect_xy, rect_width, rect_height, facecolor='blue', alpha=0.5)
+                    rects.append(rect)
+                    ax.add_patch(rect)
+                    # plt.pause(0.001)
+                else:
+                    flag = False
+                    for i, freq in enumerate(noise_freq):
+                        if i % h == 0:
+                            if freq - noise_freq[i - h] == h:
+                                rect_height += 20000 / len(noise_freq) * h
+                                if not flag:
+                                    rect_y = frequencies[freq]
+                                    flag = True
+                            else:
+                                if flag:
+                                    print('im here')
+                                    rect_xy = (rect_x, rect_y)
+                                    rect = plt.Rectangle(rect_xy, rect_width, rect_height, facecolor='blue', alpha=0.5)
+                                    rects.append(rect)
+                                    ax.add_patch(rect)
+                                    flag = False
+                                else:
+                                    rect_y = frequencies[freq]
+                                    rect_xy = (rect_x, rect_y)
+                                    rect_height = 20000 / len(noise_freq) * 20
+                                    rect = plt.Rectangle(rect_xy, rect_width, rect_height, facecolor='blue', alpha=0.5)
+                                    rects.append(rect)
+                                    ax.add_patch(rect)
+                                    flag = False
 
-rects = []
+                    plt.pause(0.003)
 
+            # print(np.arange(1, 1025))
+            # for noise_freq in noise_freqs:
+                # if noise_freq != np.arange(1, 1025):
+                #     print('yooooo')
+            # rect_y += rect_height
+            #
+            # rect_xy = (rect_x, rect_y)
+            # rect = plt.Rectangle(rect_xy, rect_width, rect_height, facecolor='blue', alpha=0.5)
+            # rects.append(rect)
+            # ax.add_patch(rect)
+            # plt.pause(0.001)
+
+# Создание кнопки
+analyze_button = tk.Button(window, text="Анализировать", command=analyze_button_click)
+
+# Размещение кнопки на окне
+analyze_button.pack()
+
+def update_brightness(val):
+    brightness = brightness_slider.val  # Получение текущего значения яркости
+    spectrogram.set_clim(vmin=brightness)  # Изменение яркости спектрограммы
+    fig.canvas.draw_idle()  # Обновление окна
+
+brightness_slider.on_changed(update_brightness)
 
 def update_spectrogram(a):
-    fig.set_facecolor('white')
-    global rect_x
-    global rects
+    while not pause:
+        fig.set_facecolor('white')
+        global rect_x, rect
+        global rects
+        global rec_dur
+        global j
+        j += 1
 
-    # Чтение аудиоданных из потока
-    data = stream.read(CHUNK)
+        # Чтение аудиоданных из потока
+        data = stream.read(CHUNK)
 
-    # Преобразование байтовых данных в числовой массив
-    audio = np.frombuffer(data, dtype=np.int16)
+        # Преобразование байтовых данных в числовой массив
+        audio = np.frombuffer(data, dtype=np.int16)
 
-    # Расчет спектрограммы
-    frequencies, times, spectrogram_data = signal.spectrogram(audio, fs=RATE, window='hann', nperseg=2048, noverlap=1024)
+        # Расчет спектрограммы
+        frequencies, _, spectrogram_data = signal.spectrogram(audio, fs=RATE, window='hann', nperseg=2048, noverlap=1024)
+        rec_dur += times[1] - times[0]
+        # print(frequencies)
 
-    y = 1500000
-    db = 20 * np.log10(abs(spectrogram_data) / y)
-    if -10 <= np.max(db) <= 0:
-        print('Опасная зона!')
-        fig.set_facecolor('yellow')
-    elif np.max(db) >= 0:
-        print('ПЕРЕГРУЗКА!!!')
-        fig.set_facecolor('red')
+        y = 1000000
+        db = np.array(20 * np.log10(abs(spectrogram_data) / y))
+        hist_db.append(db)
+        # print(len(db))
+        if -10 <= np.max(db) <= 0:
+            fig.set_facecolor('yellow')
+        if np.max(db) >= 0:
+            fig.set_facecolor('red')
+        print(len(hist_db))
+        # Перевод амплитуды в дБ
+        spectrogram_data = 20 * np.log10(spectrogram_data/100)
 
-    # Перевод амплитуды в дБ
-    spectrogram_data = 20 * np.log10(spectrogram_data/100)
+        # Настройка цветовой карты для выделения шумов
+        spectrogram.set_cmap('inferno')  # Используйте любую цветовую карту, которая подходит вам
 
-    # Обновление массива со столбцами спектрограммы
-    columns[:, :-1] = columns[:, 1:]
-    columns[:, -1] = spectrogram_data[:int(MAX_FREQUENCY), 0]
-
-    # Обновление спектрограммы
-    spectrogram.set_array(columns)
-
-    # Поиск областей с положительной громкостью
-    positive_areas = np.where(spectrogram_data[:int(MAX_FREQUENCY), :] > 0)
-
-    # Создание новых квадратов в местах скачков амплитуды
-    for freq_idx, col_idx in zip(positive_areas[0], positive_areas[1]):
-        if len(rects) > 4:
-            break
-        elif len(rects) == 0:
-            rect_x = 0.05  # Координата x (время) квадрата
-            rect_y = frequencies[freq_idx]  # Начальная координата y (частота) квадрата
-            rect_width = -0.001  # Ширина квадрата
-            rect_height = 200  # Высота квадрата (диапазон частоты)
-            rect_xy = (rect_x, rect_y)
-
-            rect = plt.Rectangle(rect_xy, rect_width, rect_height, facecolor='white', alpha=0.8)
-            ax.add_patch(rect)
-            rects.append(rect)
-        elif len(rects) > 0:
-            rect_x = 0.05  # Координата x (время) квадрата
-            rect_y = frequencies[freq_idx]  # Начальная координата y (частота) квадрата
-            rect_width = -0.001  # Ширина квадрата
-            rect_height = 200  # Высота квадрата (диапазон частоты)
-            rect_xy = (rect_x, rect_y)
-            for rect in rects:
-                if abs(rect_y - rect.get_x()) > 200:
-                    rect = plt.Rectangle(rect_xy, rect_width, rect_height, facecolor='white', alpha=0.8)
-                    ax.add_patch(rect)
-                    rects.append(rect)
-
-                    # print(len(rects))
+        columns[:, :-1] = columns[:, 1:]
+        columns[:, -1] = spectrogram_data[:int(MAX_FREQUENCY), 0]
+        # Обновление спектрограммы
+        spectrogram.set_array(columns)
 
 
-    if len(rects) > 0:
-        new_rects = []
-        for rect in rects:
-            if rect.get_x() > 0.023:
-                rect.set_x(rect.get_x() - 0.023 / NUM_COLUMNS)
-                print(rect.get_x())
-                new_rects.append(rect)
-        rects = new_rects
 
+        return (spectrogram)
 
-    return (spectrogram, *rects)
 
 def animate_spectrogram():
     animation = FuncAnimation(fig, update_spectrogram, frames=None, interval=0, blit=False)
@@ -144,4 +207,10 @@ def stop_stream():
     stream.close()
     p.terminate()
 
-# animate_spectrogram()
+# Start the animation
+animate_spectrogram()
+
+
+
+# Запуск главного цикла обработки событий окна
+window.mainloop()
